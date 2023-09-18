8fa67b34258bcb5d95ca5c904f14e532b0e1fd3b
yugmldnjs <gmldnjs0317@gmail.com>
2023-09-18 16:36:47 +0900

    Week3.1. 반복문

---
+++ challenge/week3/do-while_even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int i = 1;
+	do {
+		// i가 짝수면 i + 1
+		if (i % 2 == 0) {
+			i++;
+		}
+		printf("%d Hello World\n", i++);  // "Hello World" 출력 후 i + 1
+	} while (i < 10);  // i가 10이 되면 반복문 종료
+	return 0;
+}
