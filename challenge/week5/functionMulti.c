4a14743d0305d30e79dca99d802e9a6a37422d4b
yugmldnjs <gmldnjs0317@gmail.com>
2023-10-06 17:38:19 +0900

    서식지정자 변경

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -5,9 +5,9 @@ long multiF(long value);  // 함수 선언
 
 // 1부터 n까지의 곱 출력, multiF 함수 호출하여 계산
 int main(void) {
-	printf("1부터 2까지의 곱은 %d\n", multiF(2));
-	printf("1부터 3까지의 곱은 %d\n", multiF(3));
-	printf("1부터 5까지의 곱은 %d\n", multiF(5));
+	printf("1부터 2까지의 곱은 %ld\n", multiF(2));
+	printf("1부터 3까지의 곱은 %ld\n", multiF(3));
+	printf("1부터 5까지의 곱은 %ld\n", multiF(5));
 }
 
 
